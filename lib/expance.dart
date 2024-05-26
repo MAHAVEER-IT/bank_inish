dc7a07b73499b4cb2f0a32d364892eaba424fc6e
Mahaveer <kannankannan778800@gmail.com>
2024-05-26 21:24:11 +0530

    expance finish

---
+++ lib/expance.dart
@@ -0,0 +1,193 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ExpenseTrackerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        hintColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: ExpenseTrackerHomePage(),
+    );
+  }
+}
+
+class ExpenseTrackerHomePage extends StatefulWidget {
+  @override
+  _ExpenseTrackerHomePageState createState() => _ExpenseTrackerHomePageState();
+}
+
+class _ExpenseTrackerHomePageState extends State<ExpenseTrackerHomePage> {
+  List<Map<String, dynamic>> _expenses = [];
+
+  final _amountController = TextEditingController();
+  final _reasonController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadExpenses();
+  }
+
+  void _addExpense() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final enteredReason = _reasonController.text;
+
+    if (enteredAmount == null || enteredReason.isEmpty) {
+      return;
+    }
+
+    final newExpense = {
+      'amount': enteredAmount,
+      'reason': enteredReason,
+      'date': DateTime.now().toString(),
+    };
+
+    setState(() {
+      _expenses.add(newExpense);
+    });
+
+    _saveExpenses();
+
+    _amountController.clear();
+    _reasonController.clear();
+    Navigator.of(context).pop();
+  }
+
+  void _startAddNewExpense(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: 'Amount'),
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Reason'),
+                  controller: _reasonController,
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  child: Text('Add Expense'),
+                  onPressed: _addExpense,
+                ),
+              ],
+            ),
+          ),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
+  Future<void> _loadExpenses() async {
+    final prefs = await SharedPreferences.getInstance();
+    final expensesData = prefs.getString('expenses');
+    if (expensesData != null) {
+      setState(() {
+        _expenses = List<Map<String, dynamic>>.from(json.decode(expensesData));
+      });
+    }
+  }
+
+  Future<void> _saveExpenses() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('expenses', json.encode(_expenses));
+  }
+
+  void _deleteExpense(int index) {
+    setState(() {
+      _expenses.removeAt(index);
+    });
+    _saveExpenses();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewExpense(context),
+          ),
+        ],
+      ),
+      body: _expenses.isEmpty
+          ? Center(
+              child: Text(
+                'No expenses added yet!',
+                style: TextStyle(fontSize: 20),
+              ),
+            )
+          : ListView.builder(
+              itemCount: _expenses.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('\₹${_expenses[index]['amount']}'),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      _expenses[index]['reason'],
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd()
+                          .format(DateTime.parse(_expenses[index]['date'])),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => _deleteExpense(index),
+                    ),
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewExpense(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
